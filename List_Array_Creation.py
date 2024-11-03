146b2fbbd1acd2d4e2a7bc151a5644b7aea56996
Beenish786 <ahb786523@gmail.com>
2024-11-03 06:21:10 -0800

    Create List_Array_Creation.py

---
+++ List_Array_Creation.py
@@ -0,0 +1,8 @@
+# Creating a list
+my_list = [1, 2, 3, "apple", "banana"]
+print("List:", my_list)
+
+# Creating an array (using NumPy library)
+import numpy as np
+my_array = np.array([4, 5, 6, 7, 8])
+print("Array:", my_array)
