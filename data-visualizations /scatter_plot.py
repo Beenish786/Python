ee148187c720bec3d16f861456ffc61e786c719e
Beenish786 <ahb786523@gmail.com>
2024-11-05 07:58:00 -0800

    Create scatter_plot.py

---
+++ data-visualizations /scatter_plot.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pollution=[32,54,45,67,98,42,82, 74]
+City=['Sahiwal', 'pindi', 'lahore', 'harapa','town','city','isalbamd','muree']
+
+plt.figure(figsize=(10, 6))
+plt.scatter(City, pollution, color='green')
+plt.show()
